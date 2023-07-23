f4be51c9a0e122f64689e6a5fa51f427981c84f0
DimaT89 <dimat89@ukr.net>
2023-07-23 22:13:02 +0300

    add key to EmployeesList

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,7 +4,10 @@ import './employees-list.css'
 
 const EmployeesList = ({data}) => {
     const elements = data.map(item => {
-        return <EmployeesListItem {...item}/>
+        const {id, ...itemProps} = item
+        return <EmployeesListItem
+            key={id}
+            {...itemProps}/>
     })
 
     return (
